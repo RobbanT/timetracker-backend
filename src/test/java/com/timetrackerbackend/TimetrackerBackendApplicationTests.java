b812fd185cdc564f128d8547b3cfb02d3a53afd0
Roberto Tencic <roberto.tencic@outlook.com>
2024-04-27 10:39:29 +0200

    Uppdatering.

---
+++ src/test/java/com/timetrackerbackend/TimetrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
