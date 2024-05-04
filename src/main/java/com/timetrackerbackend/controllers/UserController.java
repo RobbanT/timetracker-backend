0718fbb36b4f9fe283a517b8b774685b760f32ba
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-04 15:46:32 +0200

    Fix.

--- src/main/java/com/timetrackerbackend/controllers/UserController.java
+++ src/main/java/com/timetrackerbackend/controllers/UserController.java
@@ -31,8 +31,6 @@ public class UserController {
 
     @PatchMapping("/user")
     public User editUser(@RequestBody User user) {
-        System.out.println("hej");
-        return null;
-        // return userService.editUser(username, tasks);
+        return userService.editUser(user);
     }
 }
