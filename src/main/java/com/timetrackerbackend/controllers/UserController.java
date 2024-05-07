116f6815206fdf3c5487308f7e842fb2fa0f9725
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-07 10:04:06 +0200

    Fix.

--- src/main/java/com/timetrackerbackend/controllers/UserController.java
+++ src/main/java/com/timetrackerbackend/controllers/UserController.java
@@ -35,6 +35,7 @@ public class UserController {
     // Skapar uppgift
     @PostMapping("/user/{username}/task/{title}")
     public Task setTask(@PathVariable String username, @PathVariable String title) {
+        System.out.println("Hej på dig");
         return setTask(username, title);
     }
 
