15b889a8a6e03362a5f9d0ab85158287bb762997
Roberto Tencic <roberto.tencic@outlook.com>
2024-05-07 18:46:23 +0200

    Fix.

--- src/main/java/com/timetrackerbackend/services/UserService.java
+++ src/main/java/com/timetrackerbackend/services/UserService.java
@@ -50,10 +50,7 @@ public class UserService {
     // Skapar uppgift. Returnerar null om en uppgift med samma titel redan
     // existerar.
     public Task setTask(String username, String title) {
-        System.out.println(username + " " + title);
-        Query query = new Query();
-        query.addCriteria(Criteria.where("username").is(username));
-        User user = mongoOperations.findOne(query, User.class);
+        User user = findUser(username);
 
         for (Task task : user.getTasks()) {
             if (task.getTitle().equals(title)) {
@@ -62,16 +59,15 @@ public class UserService {
         }
 
         user.getTasks().add(new Task(title));
-        mongoOperations.updateFirst(query, Update.update("tasks", user.getTasks()),
+        mongoOperations.updateFirst(new Query().addCriteria(Criteria.where(username)
+                .is(username)), Update.update("tasks", user.getTasks()),
                 User.class);
 
         return new Task(title);
     }
 
     public Task deleteTask(String username, String title) {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("username").is(username));
-        User user = mongoOperations.findOne(query, User.class);
+        User user = findUser(username);
 
         for (Task task : user.getTasks()) {
             if (task.getTitle().equals(title)) {
@@ -80,7 +76,7 @@ public class UserService {
         }
 
         user.getTasks().removeIf((task) -> task.getTitle().equals(title));
-        return new Task("hej");
+        return new Task(username);
     }
 
     public Task editTask(Task task) {
